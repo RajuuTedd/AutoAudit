060b246ce98944657a9f9e58a4dcf2357906826f
RajuuTedd <siddharthbabel082@gmail.com>
2025-07-29 20:07:49 +0530

    Initial commit for AutoAudit BE project

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const scanRoutes = require("./routes/scanRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/scan", scanRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() =>
+    app.listen(5000, () => console.log("Server running on port 5000"))
+  )
+  .catch((err) => console.error("MongoDB connection error:", err));
